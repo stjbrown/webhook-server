92d5be8bbb5be72f992ebc31855a791345e9666f
root <stjbrown@gmail.com>
2018-01-08 14:52:19 +0000

    user scripts added

---
+++ scripts/spCreate.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#Log into Azure
+
+AZURE_SUBSCRIPTION_ID=$(az login -u $AZURE_USERNAME -p $AZURE_PW | jq '.[0] .id' --raw-output)
+AZURE_SUBSCRIPTION_ID="export AZURE_SUBSCRIPTION_ID=${AZURE_SUBSCRIPTION_ID}"
+
+#GET Tenat  ID
+AZURE_TENANT=$(az account list | jq '.[0] .tenantId' --raw-output)
+AZURE_TENANT="export AZURE_TENANT=${AZURE_TENANT}"
+
+#Create new SP
+AZURE_CLIENT_ID=$(az ad sp create-for-rbac --name $1 --password $2 | jq '.appId' --raw-output)
+AZURE_CLIENT_ID="export AZURE_CLIENT_ID= ${AZURE_CLIENT_ID}"
+AZURE_SECRET="export AZURE_SECRET=${2}"
+
+echo $AZURE_SUBSCRIPTION_ID
+echo $AZURE_TENANT
+echo $AZURE_CLIENT_ID
+echo $AZURE_SECRET
